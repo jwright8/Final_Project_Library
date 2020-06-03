d2829b97f8da4e2e214936f6116cda28b3485f9c
Jonathan Wright <jwright8@uoregon.edu>
2020-06-03 10:13:35 -0700

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,10 @@
+
+
+def hello():
+  print("Hello!")
+
+# For functions here to work, the library must be imported at the top of the file. Havign a gist for this would be great to just copy/paste.
+  
+# Functions used in the final project
+# To use these, use import that we did for UO_Puddles but change to have features from this library. Note that the library is the folder and the functions.py is the file
+# import Final_Project_Library.functions as sf
